384f3e029f35448eec3f2526d6df2220a8621104
NET <NET@DESKTOP-NSMUMFG>
2022-10-22 11:00:10 +0600

    Add project files.

---
+++ While Loop Program Print Hello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop_Program_Print_Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Hello");
+            }
+            Console.ReadKey();
+        }
+    }
+}
